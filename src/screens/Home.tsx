f85adaff23013af5d1a6a568a2a50794b317c5d0
cristianhd <cristian.hd1996@gmail.com>
2021-08-23 19:14:40 -0300

    init project

---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Container } from "./Styled";
+
+const Home = ()=>{
+    return(
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Home;
