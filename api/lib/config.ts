5e66f6403eb407fbfb57cb1f2e4f1780ced62f80
cristianhd <cristian.hd1996@gmail.com>
2021-08-25 12:49:19 -0300

    change config.ts

--- api/lib/config.ts
+++ api/lib/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
   port: process.env.API_PORT || "3001",
-  cors: process.env.CORS || "localhost:3000",
+  url: process.env.URL || "localhost:3001",
 };
 
 export default config;
